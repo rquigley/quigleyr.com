f5dc4f67064eda1bc841b447ec0b8c5f71a0a700
Ryan Quigley <ryan@auditre.co>
2024-04-12 18:07:13 -0400

    HP content updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
 import { Playfair_Display } from "next/font/google";
-import Image from "next/image";
+
+// import { Posts } from "./posts";
 
 const playfair = Playfair_Display({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
   return (
-    <div
-      className={`pt-48 text-xl leading-snug md:text-4xl md:leading-relaxed ${playfair.className}`}
-    >
-      Ryan Quigley is a software engineer and entrepreneur. I&rsquo;m passionate
-      about scaling ideas into profitable businesses.
-    </div>
+    <>
+      <p
+        className={`pt-48 text-xl leading-snug md:text-4xl md:leading-relaxed ${playfair.className}`}
+      >
+        I&rsquo;m Ryan, a software engineer and entrepreneur.
+      </p>
+      <p className="mt-8">
+        Over the past two decades I&rsquo;ve helped build and scale four
+        companies, two of them to acquisition. I&rsquo;m currently on the search
+        for something new.
+      </p>
+
+      {/* <div className="pt-20">
+        <Posts posts={[]} />
+      </div> */}
+    </>
   );
 }
